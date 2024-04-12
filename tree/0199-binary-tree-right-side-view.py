08662af78e22db662f5e0c697333ff682d0fb2a2
evelynliu <1821584441@qq.com>
2024-04-12 11:31:12 -0500

    tree part 2

---
+++ tree/0199-binary-tree-right-side-view.py
@@ -0,0 +1,23 @@
+# Very similar to level order traversal, 
+# but only add the rightmost node of each level to the result list
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            rightSide = None
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        
+        return res
