6fcf0e223dcbc14467852d84f1ae331c4b195252
evelynliu <1821584441@qq.com>
2024-04-22 10:38:50 -0500

    linked list part 2

---
+++ linked-list/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldToCopy = { None : None } # edge case
+
+        curr = head
+        while curr:
+            copy = Node(curr.val)
+            oldToCopy[curr] = copy
+            curr = curr.next
+        
+        curr = head
+        while curr:
+            copy = oldToCopy[curr]
+            copy.next = oldToCopy[curr.next]
+            copy.random = oldToCopy[curr.random]
+            curr = curr.next
+        
+        return oldToCopy[head]
