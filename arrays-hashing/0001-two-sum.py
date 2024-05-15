9af5e2c760dc3411dcf38465def0eddc718f055d
evelynliu <1821584441@qq.com>
2024-05-15 10:50:52 -0500

    arrays and hashing

---
+++ arrays-hashing/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in map:
+                return [i, map[diff]]
+            
+            map[n] = i
