8610ee32768afcb2df5afc158936cb84c63132ce
evelynliu <1821584441@qq.com>
2024-06-10 16:20:22 -0500

    arrays and hashing part 4

---
+++ arrays-hashing/0238-product-of-array-except-self.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums) # [1, 2, 3, 4]
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        # res = [1, 1, 2, 6] prefixes
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        # res = [24, 12, 8, 6] result
+
+        return res 
---
+++ arrays-hashing/0271-encode-and-decode-strings.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            length = len(s)
+            res += str(length) + "#" + s
+        
+        return res
+
+    # 4#neet4#code4#love3#you
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+        
+        return res
