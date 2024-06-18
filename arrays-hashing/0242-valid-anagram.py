0f88be22e772062159c9487ad7bc42fc5f7ef3d5
evelynliu <1821584441@qq.com>
2024-06-18 16:19:32 -0500

    mock interview

--- arrays-hashing/0242-valid-anagram.py
+++ arrays-hashing/0242-valid-anagram.py
@@ -10,3 +10,18 @@ class Solution:
             countT[t[i]] = 1 + countT.get(t[i], 0)
         
         return countS == countT
+    
+# mock
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        smap = {} # letter -> frequency
+        tmap = {}
+
+        for i in range(len(s)):
+            smap[s[i]] = smap.get(s[i], 0) + 1
+            tmap[t[i]] = tmap.get(t[i], 0) + 1
+        
+        return True if smap == tmap else False
--- linked-list/0141-linked-list-cycle.py
+++ linked-list/0141-linked-list-cycle.py
@@ -9,3 +9,16 @@ class Solution:
                 return True
         
         return False
+
+# mock
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
